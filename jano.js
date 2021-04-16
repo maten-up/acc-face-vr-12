f16cbf02ff186a821f5cc5e1976bc2213ff240b3
maten-up <82652611+maten-up@users.noreply.github.com>
2021-04-16 14:22:39 +0300

    Update jano.js

--- jano.js
+++ jano.js
@@ -1,6 +1,6 @@
 
 const Discord = require("discord.js");
-const myid = ['id xot'] ////////////////////prefix wara 
+const myid = ['762335886259257365'] ////////////////////prefix wara 
 const jano = new Discord.Client();
 const jano2 = new Discord.Client();
 const jano3 = new Discord.Client();
@@ -7392,7 +7392,7 @@ jano200.on("ready", () => {
 
 //////////////// tokn lera da bne 
 
-jano.login("");
+jano.login("ODMyMzg0MjIzODYzODk4MTIz.YHjAsw.I16JwDuIBiVhjgSTVXmgGRlSr-g");
 jano2.login("");
 jano3.login("");
 jano4.login("");
